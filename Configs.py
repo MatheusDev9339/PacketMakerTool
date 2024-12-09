095bc54a886a2c9940c359a0cb6b13469c87e846
Matheus <125626334+MatheusDev9339@users.noreply.github.com>
2024-12-09 00:37:23 -0300

    Create Configs.py

---
+++ Configs.py
@@ -0,0 +1,4 @@
+configs = {
+    'directory_server': 'C:/Users/Matheus_H/Desktop/Alfa Diesel - Server/Classes/Packets/Server/',
+    'directory_client': 'C:/Users/Matheus_H/Desktop/Alfa Diesel - Server/Classes/Packets/Client/'
+}
